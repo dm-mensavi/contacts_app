d2c2a43ad1e510360ac54b7530d59540e1b01104
godfr <gowusu018@gmail.com>
2025-03-10 20:04:33 +0100

    background picture edited

--- src/main/java/org/project/contactapp/controllers/HomePageController.java
+++ src/main/java/org/project/contactapp/controllers/HomePageController.java
@@ -1,12 +1,57 @@
+//package org.project.contactapp.controllers;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.image.Image;
+//import javafx.scene.image.ImageView;
+//import org.project.contactapp.MainApp;
+//
+//public class HomePageController {
+//    @FXML
+//    private ImageView imageView;
+//
+//    public HomePageController() {
+//        // Empty constructor for now; can be used for future dependencies if needed
+//        Image image = new Image(getClass().getResourceAsStream("/images/bg.png"));
+//        imageView.setImage(image);
+//    }
+//
+//    @FXML
+//    protected void onViewContactsClick() {
+//        System.out.println("View Contacts Clicked!");
+//        MainApp.navigateTo("allContacts-page.fxml"); // Navigate to the "View Contacts" page
+//    }
+//
+//    @FXML
+//    protected void onAddContactClick() {
+//        System.out.println("Add Contact Clicked!");
+//        MainApp.navigateTo("addContact-page.fxml"); // Navigate to the "Add Contact" page
+//    }
+//}
+
 package org.project.contactapp.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.project.contactapp.MainApp;
 
 public class HomePageController {
+    @FXML
+    private ImageView imageView;
 
     public HomePageController() {
         // Empty constructor for now; can be used for future dependencies if needed
+    }
+
+    @FXML
+    public void initialize() {
+        // Ensure imageView is initialized before setting the image
+        if (imageView != null) {
+            Image image = new Image(getClass().getResource("/org/project/contactapp/images/background.png").toExternalForm());
+            imageView.setImage(image);
+        } else {
+            System.out.println("ImageView is null. Check FXML binding.");
+        }
     }
 
     @FXML
