ffe30b1ffbf1105b27ff2eac4339147a6100fd2b
godfr <gowusu018@gmail.com>
2025-03-10 21:05:12 +0100

    changed background

--- src/main/java/org/project/contactapp/controllers/HomePageController.java
+++ src/main/java/org/project/contactapp/controllers/HomePageController.java
@@ -1,33 +1,3 @@
-//package org.project.contactapp.controllers;
-//
-//import javafx.fxml.FXML;
-//import javafx.scene.image.Image;
-//import javafx.scene.image.ImageView;
-//import org.project.contactapp.MainApp;
-//
-//public class HomePageController {
-//    @FXML
-//    private ImageView imageView;
-//
-//    public HomePageController() {
-//        // Empty constructor for now; can be used for future dependencies if needed
-//        Image image = new Image(getClass().getResourceAsStream("/images/bg.png"));
-//        imageView.setImage(image);
-//    }
-//
-//    @FXML
-//    protected void onViewContactsClick() {
-//        System.out.println("View Contacts Clicked!");
-//        MainApp.navigateTo("allContacts-page.fxml"); // Navigate to the "View Contacts" page
-//    }
-//
-//    @FXML
-//    protected void onAddContactClick() {
-//        System.out.println("Add Contact Clicked!");
-//        MainApp.navigateTo("addContact-page.fxml"); // Navigate to the "Add Contact" page
-//    }
-//}
-
 package org.project.contactapp.controllers;
 
 import javafx.fxml.FXML;
@@ -45,15 +15,13 @@ public class HomePageController {
 
     @FXML
     public void initialize() {
-        // Ensure imageView is initialized before setting the image
-        if (imageView != null) {
+        try {
             Image image = new Image(getClass().getResource("/org/project/contactapp/images/background.png").toExternalForm());
             imageView.setImage(image);
-        } else {
-            System.out.println("ImageView is null. Check FXML binding.");
+        } catch (NullPointerException e) {
+            System.out.println("Error: Image file not found. Check resource path.");
         }
     }
-
     @FXML
     protected void onViewContactsClick() {
         System.out.println("View Contacts Clicked!");
